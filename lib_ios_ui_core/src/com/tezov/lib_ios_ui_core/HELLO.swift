5a22d1c7cb07f0352b66b0388eb950d463b885c1
tezov <tezov.app@gmail.com>
2023-08-20 18:34:02 +0200

    initial commit

---
+++ lib_ios_ui_core/src/com/tezov/lib_ios_ui_core/HELLO.swift
@@ -0,0 +1,13 @@
+//
+//  HELLO.swift
+//  lib_ios_app_core
+//
+//  Created by prive on 20/08/2023.
+//
+
+import Foundation
+import lib_ios_core
+
+public func helloUiCore() -> String {
+    return "hello ui core and \(helloIosCore())"
+}
